f1080b789f77d21a810096c7cda241306a3dd135
zh.fang <ian.zh.fang@gmail.com>
2017-06-21 22:22:52 +0800

    111

--- zh.fang.stu.callback.client/App_Start/RouteConfig.cs
+++ zh.fang.stu.callback.client/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace zh.fang.stu.callback.client
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 name: "Default",
--- zh.fang.stu.callback.server/App_Start/RouteConfig.cs
+++ zh.fang.stu.callback.server/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace zh.fang.stu.callback.server
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapMvcAttributeRoutes();
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
