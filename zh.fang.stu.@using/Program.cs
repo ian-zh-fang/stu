8317a93bfddb968417f80a477a094ddb6e6a8eae
振 方 <ian.zh.fang@gmail.com>
2017-12-15 17:18:43 +0800

    新增 using 关键字测试

---
+++ zh.fang.stu.@using/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zh.fang.stu._using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var first = new FirstDisable())
+            using (var second = new SecondDisable())
+            using (var third = new ThirdDisable())
+            {
+                Console.WriteLine("invoked --> {0}", typeof(Program).GetMethod(nameof(Main), System.Reflection.BindingFlags.Static| System.Reflection.BindingFlags.NonPublic).Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class BaseDisable : IDisposable
+    {
+        void IDisposable.Dispose()
+        {
+            Console.WriteLine("disposed --> {0}", GetType().FullName);
+        }
+    }
+
+    class FirstDisable : BaseDisable
+    {
+
+    }
+
+    class SecondDisable : BaseDisable
+    {
+
+    }
+
+    class ThirdDisable : BaseDisable
+    {
+
+    }
+}
